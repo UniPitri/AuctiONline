57be55ce9aaa7ea4b1bf987e781633df59dc84eb
LorisLorenzini <loris.lorenzini16@gmail.com>
2022-05-16 20:31:23 +0200

    Aggiunta mongoose models per le collezioni Aste e Studenti

---
+++ app/backend/models/utente.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+//Imposto il modello mongoose degli utenti
+module.exports = mongoose.model('Utenti', new Schema({ 
+	Username: String,
+    Password: String,
+    Salt: String,
+    Mail: String,
+    AstePreferite: [Schema.Types.ObjectId]
+}));
