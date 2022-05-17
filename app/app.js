765e5df63c98c85c9679cc5fa4f400c4c7bef38a
LorisLorenzini <loris.lorenzini16@gmail.com>
2022-05-17 10:07:37 +0200

    Aggiunto app.js, con connessione DB, startup del server e route di esempio per pagina principale

---
+++ app/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+const mongoose = require('mongoose');
+
+//Configurazione parsing middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+//Definisco visibilità esterna
+app.use(express.static(__dirname+'/frontend'))
+
+//Get nel caso '/'
+app.get('/', function(req, res){
+    res.sendFile(__dirname+'/frontend/prova.html')
+});
+
+//Configurazione mongoose e avvio server
+app.locals.db = mongoose.connect("mongodb+srv://Pitri:wf1PhiJyzLsqulb6@cluster0.00kap.mongodb.net/auctionline?retryWrites=true&w=majority",
+    {useNewUrlParser: true, useUnifiedTopology: true})
+.then( () => {
+    console.log("Connected to Database");
+
+    app.listen(3000, function() {
+    console.log('Server running on port ', 3000);
+    });
+});
