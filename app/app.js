e9b28b0b52a9195d14da751e02852bac9ec0ede4
LorisLorenzini <loris.lorenzini16@gmail.com>
2022-05-18 09:56:15 +0200

    Aggiunte api per la visione del catalogo e piccola modifica ad app.js

--- app/app.js
+++ app/app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 const autenticazione = require('./autenticazione.js');
 
+const aste = require('./aste.js')
+
 //Configurazione parsing middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -11,18 +13,23 @@ app.use(express.urlencoded({extended: true}))
 //Definisco visibilità esterna
 app.use(express.static('../static'))
 
+/**
+ * Serve front-end static files
+ */
+ app.use('/', express.static(process.env.FRONTEND || 'static'));
+ // If process.env.FRONTEND folder does not contain index.html then use the one from static
+ app.use('/', express.static('static')); // expose also this folder
+
 
 app.use((req,res,next) => {
-    console.log(req.method + ' ' + req.url)
-    next()
+    console.log(req.method + ' ' + req.url);
+    next();
 })
+
 
 app.use('/api/v1/autenticazione', autenticazione);
 
-//Get nel caso '/'
-/* app.get('/', function(req, res){
-    res.sendFile(__dirname+'/frontend/prova.html')
-}); */
+app.use('/api/v1/aste', aste);
 
 //Configurazione mongoose e avvio server
 app.locals.db = mongoose.connect(process.env.DB_URL,
@@ -34,14 +41,3 @@ app.locals.db = mongoose.connect(process.env.DB_URL,
     console.log('Server running on port ', 3000);
     });
 });
-
-/*app.listen(3000, function() {
-    console.log('Server running on port ', 3000);
-    });*/
-
-/**
- * Serve front-end static files
- */
-app.use('/', express.static(process.env.FRONTEND || 'static'));
-// If process.env.FRONTEND folder does not contain index.html then use the one from static
-app.use('/', express.static('static')); // expose also this folder
