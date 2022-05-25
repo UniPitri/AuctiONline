b031d4456214271fdcc2c2837df35ec57d0cdd7f
UniPitri <pietro.ferrari@studenti.unitn.it>
2022-05-25 16:46:17 +0200

    Preferiti UI
    Creato pannello laterale che si mostra con le card solo a chi è loggato

--- static/script.js
+++ static/script.js
@@ -1,120 +1,8 @@
 var categoriaClick = 0;
 
-function login() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-
-    if(username == ""){
-        window.alert("Devi inserire un username");
-    }
-    else if(password == ""){
-        window.alert("Devi inserire una password");
-    }
-    else{
-        fetch('../api/v1/autenticazione', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify( { username: username, password: password } ),
-        })
-        .then((resp) => resp.json())
-        .then(function(data) {
-            if(data.success) {
-                sessionStorage.setItem("token",data.token);
-                sessionStorage.setItem("email",data.email);
-                sessionStorage.setItem("id",data.id);
-                sessionStorage.setItem("self",data.self);
-                window.location.href = "index.html";
-            } else {
-                document.getElementById('message').innerHTML = data.message;
-                $('#alert').modal('show');
-            }
-        })
-        .catch(error => console.error(error));
-    }
-};
-
-function logout(){
-    sessionStorage.removeItem("token");
-    sessionStorage.removeItem("email");
-    sessionStorage.removeItem("id");
-    sessionStorage.removeItem("self");
-    window.location.href = "index.html";
-}
-
-function register() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var email = document.getElementById("email").value;
-
-    if (username == ""){
-        window.alert("E' richiesto l'username!");
-    }
-    else if(password == ""){
-        window.alert("E' richiesta la password!");
-    }
-    else if(email == ""){
-        window.alert("E' richiesta la mail!");
-    }
-    else{
-        fetch('../api/v1/registrazione', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify( { username: username, password: password, email:email } ),
-        })
-        .then((resp) => resp.json())
-        .then(function(data) {
-            if(data.success) {
-                sessionStorage.setItem("token",data.token);
-                sessionStorage.setItem("email",data.email);
-                sessionStorage.setItem("id",data.id);
-                sessionStorage.setItem("self",data.self);
-                window.location.href = "index.html";
-            } else {
-                document.getElementById('message').innerHTML = data.message;
-                $('#alert').modal('show');
-            }
-        })
-        .catch( error => console.error(error) );
-    }
-}
-
-function offerta(asta, prezzo) {
-    if(sessionStorage.getItem('id') == null)
-        window.location.href = 'login.html';
-    else if(prezzo.value %1 != 0 && prezzo.value.split('.')[1].length > 2) {
-        document.getElementById('modalContent').className = 'modal-content bg-danger';
-        document.getElementById('modalTitle').innerHTML = 'Errore';
-        document.getElementById('message').innerHTML = 'Massimo due decimali';
-        $('#alert').modal('show');
-    } else
-        fetch(asta, {
-            method: 'PUT',
-            headers: {
-                'x-access-token': sessionStorage.getItem("token"),
-                'id-account': sessionStorage.getItem("id"),
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify( { prezzo: prezzo.value } ),
-        })
-        .then((resp) => resp.json())
-        .then(function(data) {
-            if(data.success) {
-                document.getElementById('modalContent').className = 'modal-content bg-success';
-                document.getElementById('modalTitle').innerHTML = 'Successo';
-            } else {
-                document.getElementById('modalContent').className = 'modal-content bg-danger';
-                document.getElementById('modalTitle').innerHTML = 'Errore';
-            }
-            
-            document.getElementById('message').innerHTML = data.message;
-            $('#alert').modal('show');
-        })
-        .catch( error => console.error(error) );
-}
-
-function redirect2(e, n) {
-    if (e.target.id != "input")
-        alert(n);
+function caricaPagina() {
+    caricaAste();
+    caricaPannelloLaterale();
 }
 
 function caricaAste() {
@@ -263,6 +151,234 @@ function caricaAste() {
         })
     })
     .catch( error => console.error(error) );
+}
+
+function caricaPannelloLaterale() {
+    if(sessionStorage.getItem("token")){
+        const cardDeck = document.getElementById('sideCardDeck');
+        fetch('../api/v1/aste', {
+            method: 'GET',
+        })
+        .then((resp) => resp.json()) // Transform the data into json
+        .then(function (data) { // Here you get the data to modify as you please        
+            return data.map(function (asta) { // Map through the results and for each run the code below
+                let container = document.createElement('div');
+                container.className = "container";
+                container.style = "margin: 0";
+                let row = document.createElement('div');
+                row.className = "row";
+                let div = document.createElement('div');
+                div.className = "card rounded";
+                //div.setAttribute('onclick', 'if(event.target.id != "input") window.location.href = "' + asta.self + '"');
+                div.style = "background-color: #38d996; cursor: pointer; margin: 1% 5%";
+                let row2 = document.createElement('div');
+                row2.className = "row no-gutters";
+                let col1 = document.createElement('div');
+                col1.className = "col-md-4";
+                let imgProdotto = document.createElement('img');
+                imgProdotto.src = "fotoProdotti/" + asta.dettagliProdotto.Foto[0];
+                imgProdotto.style = "max-width: 100%";
+                let col2 = document.createElement('div');
+                col2.className = "col-md-8";
+                let div2 = document.createElement('div');
+                div2.className = "card-body";
+                let h5 = document.createElement('h5');
+                h5.className = "card-title";
+                h5.innerHTML = asta.dettagliProdotto.Nome;
+                let p = document.createElement('p');
+                p.className = "card-text";
+                p.innerHTML = "Loading...";
+                let p2 = document.createElement('p');
+                p2.className = "card-text";
+                p2.innerHTML = "Loading...";
+    
+                var x = setInterval(function () {
+                    var now = new Date().getTime();
+    
+                    if (new Date(asta.dettagliAsta.Inizio).getTime() > now) {
+                        if (asta.dettagliAsta.PrezzoMinimo != null)
+                            p.innerHTML = "Prezzo minimo: " + asta.dettagliAsta.PrezzoMinimo + "€";
+                        else
+                            p.innerHTML = "Prezzo minimo: X";
+    
+                        p2.innerHTML = "L'asta inizierà tra: ";
+                        countDownDate = new Date(asta.dettagliAsta.Inizio).getTime();
+                    } else {
+                        if (asta.dettagliAsta.Offerte.length != 0){
+                            p.innerHTML = "Prezzo attuale: " + asta.dettagliAsta.Offerte[asta.dettagliAsta.Offerte.length-1] + "€";
+                        }
+                        else{
+                            p.innerHTML = "Prezzo attuale: X";
+                        }
+                        p2.innerHTML = "Tempo rimanente: ";
+                        countDownDate = new Date(asta.dettagliAsta.Fine).getTime();
+                    }
+    
+                    var distance = countDownDate - now;
+                    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                    p2.innerHTML = ((new Date(asta.dettagliAsta.Inizio).getTime() > now) ? "L'asta inizierà tra: " : "Tempo rimanente: ") + days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    
+                    if (distance < 0) {
+                        clearInterval(x);
+                        p2.innerHTML = "EXPIRED";
+                    }
+                }, 1000);
+
+                div2.appendChild(h5);
+                div2.appendChild(p);
+                div2.appendChild(p2);
+                col2.appendChild(div2);            
+                col1.appendChild(imgProdotto);
+                row2.appendChild(col1);
+                row2.appendChild(col2);
+                div.appendChild(row2);
+    
+                if (sessionStorage.getItem("token")) {
+                    var img = document.createElement('img');
+                    div.appendChild(img);
+                    if(asta.preferenze != null && asta.preferenze.includes(sessionStorage.getItem("id"))){
+                        img.src = '/icone/Plain_Yellow_Star.png';
+                    }
+                    else{
+                        img.onclick = function () {aggiungiPreferita(asta.idAsta) };
+                        img.src = '/icone/star-empty.webp';
+                    }  
+                    img.id = "star"+asta.idAsta;                  
+                    img.style.height = '20px';
+                    img.style.width = '20px';
+                    img.style.width = '20px';
+                    img.style.position= 'absolute';
+                    img.style.top= '5px';
+                    img.style.right= '5px';
+                }
+                row.appendChild(div);
+                container.appendChild(row);
+                cardDeck.appendChild(container);
+            })
+        })
+        .catch( error => console.error(error) );
+    } else
+        console.log("sloggato");
+}
+
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if(username == ""){
+        window.alert("Devi inserire un username");
+    }
+    else if(password == ""){
+        window.alert("Devi inserire una password");
+    }
+    else{
+        fetch('../api/v1/autenticazione', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify( { username: username, password: password } ),
+        })
+        .then((resp) => resp.json())
+        .then(function(data) {
+            if(data.success) {
+                sessionStorage.setItem("token",data.token);
+                sessionStorage.setItem("email",data.email);
+                sessionStorage.setItem("id",data.id);
+                sessionStorage.setItem("self",data.self);
+                window.location.href = "index.html";
+            } else {
+                document.getElementById('message').innerHTML = data.message;
+                $('#alert').modal('show');
+            }
+        })
+        .catch(error => console.error(error));
+    }
+};
+
+function logout(){
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("email");
+    sessionStorage.removeItem("id");
+    sessionStorage.removeItem("self");
+    window.location.href = "index.html";
+}
+
+function register() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var email = document.getElementById("email").value;
+
+    if (username == ""){
+        window.alert("E' richiesto l'username!");
+    }
+    else if(password == ""){
+        window.alert("E' richiesta la password!");
+    }
+    else if(email == ""){
+        window.alert("E' richiesta la mail!");
+    }
+    else{
+        fetch('../api/v1/registrazione', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify( { username: username, password: password, email:email } ),
+        })
+        .then((resp) => resp.json())
+        .then(function(data) {
+            if(data.success) {
+                sessionStorage.setItem("token",data.token);
+                sessionStorage.setItem("email",data.email);
+                sessionStorage.setItem("id",data.id);
+                sessionStorage.setItem("self",data.self);
+                window.location.href = "index.html";
+            } else {
+                document.getElementById('message').innerHTML = data.message;
+                $('#alert').modal('show');
+            }
+        })
+        .catch( error => console.error(error) );
+    }
+}
+
+function offerta(asta, prezzo) {
+    if(sessionStorage.getItem('id') == null)
+        window.location.href = 'login.html';
+    else if(prezzo.value %1 != 0 && prezzo.value.split('.')[1].length > 2) {
+        document.getElementById('modalContent').className = 'modal-content bg-danger';
+        document.getElementById('modalTitle').innerHTML = 'Errore';
+        document.getElementById('message').innerHTML = 'Massimo due decimali';
+        $('#alert').modal('show');
+    } else
+        fetch(asta, {
+            method: 'PUT',
+            headers: {
+                'x-access-token': sessionStorage.getItem("token"),
+                'id-account': sessionStorage.getItem("id"),
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify( { prezzo: prezzo.value } ),
+        })
+        .then((resp) => resp.json())
+        .then(function(data) {
+            if(data.success) {
+                document.getElementById('modalContent').className = 'modal-content bg-success';
+                document.getElementById('modalTitle').innerHTML = 'Successo';
+            } else {
+                document.getElementById('modalContent').className = 'modal-content bg-danger';
+                document.getElementById('modalTitle').innerHTML = 'Errore';
+            }
+            
+            document.getElementById('message').innerHTML = data.message;
+            $('#alert').modal('show');
+        })
+        .catch( error => console.error(error) );
+}
+
+function redirect2(e, n) {
+    if (e.target.id != "input")
+        alert(n);
 }
 
 function nuovaAsta(){
