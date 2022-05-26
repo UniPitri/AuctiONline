c8a477cd6c11a8b5304999aac05dffc42be95936
LorisLorenzini <loris.lorenzini16@gmail.com>
2022-05-26 14:55:36 +0200

    Tolte dimenticanze API aste vinte

--- app/utenti.js
+++ app/utenti.js
@@ -5,19 +5,17 @@ const Utente = require('./models/utente');
 
 router.get('/:id/asteVinte', async function(req, res){
     let aste = await Asta.find({'DettagliAsta.Offerenti.0':req.params.id,'DettagliAsta.Fine':{$lte: new Date()}}).exec();
-    
+
     aste = aste.map( (asta) => {
-        if(asta.DettagliAsta.Offerenti[asta.DettagliAsta.Offerenti.length-1].toString() == req.params.id){
-            return {
-                self: '/api/v1/aste/' + asta._id,
-                idAsta: asta._id,
-                dettagliProdotto: asta.DettagliProdotto,
-                inizioAsta: asta.DettagliAsta.Inizio,
-                fineAsta: asta.DettagliAsta.Fine,
-                tipoAsta: asta.DettagliAsta.Tipo,
-                offertaVincente: asta.DettagliAsta.Offerte[asta.DettagliAsta.Offerte.length-1]
-            };
-        }
+        return {
+            self: '/api/v1/aste/' + asta._id,
+            idAsta: asta._id,
+            dettagliProdotto: asta.DettagliProdotto,
+            inizioAsta: asta.DettagliAsta.Inizio,
+            fineAsta: asta.DettagliAsta.Fine,
+            tipoAsta: asta.DettagliAsta.Tipo,
+            offertaVincente: asta.DettagliAsta.Offerte[0]
+        };
     });
     res.status(200).json(aste);
 });
