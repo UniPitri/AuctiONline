18ffdac9b3a54432e3d628504b5db08e5fd5b835
LorisLorenzini <loris.lorenzini16@gmail.com>
2022-06-06 13:47:20 +0200

    Tolti log da console

--- app/app.js
+++ app/app.js
@@ -16,10 +16,10 @@ const utenti = require('./utenti.js');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use((req,res,next) => {
+/* app.use((req,res,next) => {
     console.log(req.method + ' ' + req.url);
     next();
-})
+}) */
 
 app.use('/tueAste.html', tokenCheckerPagine);
 app.use('/creazioneAsta.html', tokenCheckerPagine);
